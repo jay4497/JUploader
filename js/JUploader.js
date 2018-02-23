7d9f1bd94a063ee55f586800bd13ac566317813f
jay4497 <jay4497@126.com>
2018-02-23 21:20:41 +0800

    add readme

--- js/JUploader.js
+++ js/JUploader.js
@@ -6,22 +6,22 @@ var JUploader = {
     init: function(options){
         var options = options || this.settings;
 
-        var overlay = $('<div class="sdy-uploader-overlay"></div>'),
-            modal = $('<div class="sdy-uploader-modal panel panel-primary"></div>'),
+        var overlay = $('<div class="j-uploader-overlay"></div>'),
+            modal = $('<div class="j-uploader-modal panel panel-primary"></div>'),
             modal_title = $('<div class="panel-heading">上传图片</div>'),
             modal_body = $('<div class="panel-body"></div>');
             modal_footer = $('<div class="panel-footer">'
-                        + '<button id="sdy-uploader-complete" class="pull-right btn btn-primary">确 定</button>'
-                        + '<button id="sdy-uploader-cancel" class="pull-right btn btn-primary">取 消</button>'
+                        + '<button id="j-uploader-complete" class="pull-right btn btn-primary">确 定</button>'
+                        + '<button id="j-uploader-cancel" class="pull-right btn btn-primary">取 消</button>'
                         + '<div class="clearfix"></div>'
                         + '</div>'),
-            buttons = $('<div class="sdy-uploader-buttons">'
-                        + '<button id="sdy-uploader-upload-picker" class="btn btn-default">选择图片</button>&nbsp;'
-                        + '<button id="sdy-uploader-upload-btn" class="btn btn-primary">上 传</button>' + 
+            buttons = $('<div class="j-uploader-buttons">'
+                        + '<button id="j-uploader-upload-picker" class="btn btn-default">选择图片</button>&nbsp;'
+                        + '<button id="j-uploader-upload-btn" class="btn btn-primary">上 传</button>' + 
                         '</div>'),
-            preview = $('<div class="sdy-uploader-preview" id="sdy-uploader-files"></div>'),
-            origin_input = $('<input type="hidden" name="sdy-uploader-origin-input" id="sdy-uploader-origin-input">'),
-            result_input = $('<input type="hidden" name="sdy-uploader-input" id="sdy-uploader-input">');
+            preview = $('<div class="j-uploader-preview" id="j-uploader-files"></div>'),
+            origin_input = $('<input type="hidden" name="j-uploader-origin-input" id="j-uploader-origin-input">'),
+            result_input = $('<input type="hidden" name="j-uploader-input" id="j-uploader-input">');
 
         modal_body.append(buttons);
         modal_body.append(preview);
@@ -34,7 +34,7 @@ var JUploader = {
         $('body').append(overlay);
 
         var state = 'pending';
-        // var filelist = $('#sdy-uploader-files');
+        // var filelist = $('#j-uploader-files');
         var uploader = WebUploader.create({
                         // swf文件路径
                         //swf: '/js/Uploader.swf',
@@ -42,7 +42,7 @@ var JUploader = {
                         server: options.url,
                         // 选择文件的按钮。可选。
                         // 内部根据当前运行是创建，可能是input元素，也可能是flash.
-                        pick: '#sdy-uploader-upload-picker',
+                        pick: '#j-uploader-upload-picker',
                         // 不压缩image, 默认如果是jpeg，文件上传前会压缩一把再上传！
                         resize: false
                         });
@@ -51,8 +51,8 @@ var JUploader = {
         uploader.on( 'fileQueued', function( file ) {
             var $li = $(
                 '<div class="col-xs-3">' + 
-                '<div id="' + file.id + '" class="sdy-uploader-file-item thumbnail">' +
-                    '<p class="sdy-uploader-file-status">等待上传</p>' + 
+                '<div id="' + file.id + '" class="j-uploader-file-item thumbnail">' +
+                    '<p class="j-uploader-file-status">等待上传</p>' + 
                     '<img>' +
                     '<p>' + file.name + '</p>' +
                 '</div></div>'
@@ -60,11 +60,11 @@ var JUploader = {
             $img = $li.find('img');
 
             $li.on('mouseenter', function(){
-                $(this).find('.sdy-uploader-file-status').css('cursor', 'pointer');
-                $(this).find('.sdy-uploader-file-status').html('删 除');
+                $(this).find('.j-uploader-file-status').css('cursor', 'pointer');
+                $(this).find('.j-uploader-file-status').html('删 除');
             });
 
-            $li.on('click', '.sdy-uploader-file-status', function(){
+            $li.on('click', '.j-uploader-file-status', function(){
                 uploader.removeFile(file, true);
                 $(this).parent().parent().remove();
             });
@@ -93,13 +93,13 @@ var JUploader = {
         });
 
         uploader.on( 'uploadSuccess', function(file, data) {
-            var pre_val = $('#sdy-uploader-input').val();
-            $('#sdy-uploader-input').val(pre_val + data.path + ',');
-            $( '#'+file.id ).find('p.sdy-uploader-file-status').text('已上传');
+            var pre_val = $('#j-uploader-input').val();
+            $('#j-uploader-input').val(pre_val + data.path + ',');
+            $( '#'+file.id ).find('p.j-uploader-file-status').text('已上传');
         });
 
         uploader.on( 'uploadError', function( file ) {
-            $( '#'+file.id ).find('p.sdy-uploader-file-status').text('上传出错');
+            $( '#'+file.id ).find('p.j-uploader-file-status').text('上传出错');
         });
 
         uploader.on( 'all', function( type ) {
@@ -112,21 +112,21 @@ var JUploader = {
             }
         });
 
-        buttons.find('#sdy-uploader-upload-btn').on('click', function(){
+        buttons.find('#j-uploader-upload-btn').on('click', function(){
             //$('#file-list').html('go upload');
             console.log('click to upload.');
             uploader.upload();
         });
 
-        modal_footer.find('#sdy-uploader-complete').on('click', function(){
-            $(options.origin_input).val($('#sdy-uploader-input').val());
-            $('.sdy-uploader-overlay').fadeOut('fast', function(){
-                $('.sdy-uploader-overlay').remove();
+        modal_footer.find('#j-uploader-complete').on('click', function(){
+            $(options.origin_input).val($('#j-uploader-input').val());
+            $('.j-uploader-overlay').fadeOut('fast', function(){
+                $('.j-uploader-overlay').remove();
             });
         });
-        modal_footer.find('#sdy-uploader-cancel').on('click', function(){
-            $('.sdy-uploader-overlay').fadeOut('fast', function(){
-                $('.sdy-uploader-overlay').remove();
+        modal_footer.find('#j-uploader-cancel').on('click', function(){
+            $('.j-uploader-overlay').fadeOut('fast', function(){
+                $('.j-uploader-overlay').remove();
             });
         });
     }
